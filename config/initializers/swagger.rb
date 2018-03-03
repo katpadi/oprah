e1da1f6d05fd8b4b31f1fb03931e734e19b4b9e0
Kat Padilla <hello@katpadi.ph>
2018-03-04 03:28:01 +0800

    Additional configs for the API

---
+++ config/initializers/swagger.rb
@@ -0,0 +1,10 @@
+# GrapeSwaggerRails.options.url      = '/swagger_doc.json'
+# GrapeSwaggerRails.options.app_url  = '/docs'
+# GrapeSwaggerRails.options.app_name = 'The Oprah Winfrey APIs'
+# GrapeSwaggerRails.options.doc_expansion = 'list'
+
+# GrapeSwaggerRails.options.api_auth     = 'basic' # Or 'bearer' for OAuth
+# GrapeSwaggerRails.options.api_key_name = 'Authorization'
+# GrapeSwaggerRails.options.api_key_type = 'header'
+# GrapeSwaggerRails.options.url      = '/swagger_doc.json'
+# GrapeSwaggerRails.options.app_url  = "http://localhost:3000"
